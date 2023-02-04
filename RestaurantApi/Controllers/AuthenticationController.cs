e032bfa95c8b7e7ff0fa38042d77dc14b4ec09b8
Samitova Vika <samitova@gmail.com>
2023-02-04 23:53:43 +0100

    1. Authentification Controller was created 2. Prototipes of Register endpoint and Login endpoint were created

---
+++ RestaurantApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Application.Services.Authentication;
+using Restaurant.Contracts.Authentication;
+using IAuthenticationService = Restaurant.Application.Services.Authentication.IAuthenticationService;
+
+namespace Restaurant.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("[action]")]   
+    public IActionResult Register(RegisterRequest request) 
+    {
+        var authenticationResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authenticationResult.Id,
+            authenticationResult.FirstName,
+            authenticationResult.LastName,
+            authenticationResult.Email,
+            authenticationResult.Token);
+
+        return Ok(response);
+    }
+
+    [HttpPost("[action]")]    
+    public IActionResult Login(LoginRequest request)
+    {
+        var loginResult = _authenticationService.Login(          
+           request.Email,
+           request.Password);
+
+        var response = new AuthenticationResponse(
+            loginResult.Id,
+            loginResult.FirstName,
+            loginResult.LastName,
+            loginResult.Email,
+            loginResult.Token);
+
+        return Ok(response);
+    }
+}
--- RestaurantApi/Program.cs
+++ RestaurantApi/Program.cs
@@ -1,25 +1,35 @@
+using Restaurant.Application;
+using Restaurant.Application.Services.Authentication;
+using Restaurant.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+{
+    builder.Services.AddAplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();   
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
