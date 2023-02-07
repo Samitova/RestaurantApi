1fd90d4568a896a566c796c85753409146e4ea34
Samitova Vika <samitova@gmail.com>
2023-02-07 11:16:55 +0100

    Bill Aggregate was added

---
+++ Restaurant.Domain/BillAggregate/Bill.cs
@@ -0,0 +1,53 @@
+﻿using Restaurant.Domain.BillAggregate.ValueObjects;
+using Restaurant.Domain.Common.Models;
+using Restaurant.Domain.Dinner.ValueObjects;
+using Restaurant.Domain.GuestAggregate.ValueObjects;
+using Restaurant.Domain.Host.ValueObjects;
+
+namespace Restaurant.Domain.BillAggregate;
+
+public sealed class Bill : AggregateRoot<BillId>
+{
+    public HostId HostId { get; }
+    public GuestId GuestId { get; }
+    public DinnerId DinnerId { get; }
+    public Price Price { get; set; }
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private Bill(
+        BillId billId,       
+        HostId hostId,
+        GuestId guestId,
+        DinnerId dinnerId,
+        Price price,
+        DateTime createdDateTime,
+        DateTime updatedDateTime)
+        : base(billId)
+    {        
+        HostId = hostId;
+        GuestId = guestId;
+        DinnerId = dinnerId;
+        Price = price;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static Bill Create(        
+        HostId hostId,
+        GuestId guestId,
+        DinnerId dinnerId,
+        Price price
+        )
+    {
+        return new(
+            BillId.CreateUnique(),           
+            hostId,
+            guestId,
+            dinnerId, 
+            price,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+
+}
---
+++ Restaurant.Domain/BillAggregate/ValueObjects/BillId.cs
@@ -0,0 +1,22 @@
+﻿using Restaurant.Domain.Common.Models;
+
+namespace Restaurant.Domain.BillAggregate.ValueObjects;
+
+public sealed class BillId : ValueObject
+{
+    public Guid Value { get; }
+    private BillId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static BillId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
