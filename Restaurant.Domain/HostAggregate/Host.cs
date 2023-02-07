ae3a4b9e53c152e4696c5b1b1758b07f6f4fabb4
Samitova Vika <samitova@gmail.com>
2023-02-07 11:16:31 +0100

    HostAggregate was added

---
+++ Restaurant.Domain/HostAggregate/Host.cs
@@ -0,0 +1,61 @@
+﻿using Restaurant.Domain.Common.Models;
+using Restaurant.Domain.Dinner.ValueObjects;
+using Restaurant.Domain.Host.ValueObjects;
+using Restaurant.Domain.Menu.ValueObjects;
+using Restaurant.Domain.UserAggregate.ValueObjects;
+
+namespace Restaurant.Domain.HostAggregate;
+
+public sealed class Host : AggregateRoot<HostId>
+{
+    private readonly List<DinnerId> _dinnerIds = new();
+    private readonly List<MenuId> _menuIds = new();
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string ProfileImage { get; }   
+    public float AverageRating { get; }    
+    public UserId UserId { get; }
+    public IReadOnlyList<DinnerId> DinnerIds => _dinnerIds.AsReadOnly();
+    public IReadOnlyList<MenuId> MenuIds => _menuIds.AsReadOnly();
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private Host(
+        HostId hostId,
+        string firstName,
+        string lastName,
+        string profileImage,
+        float averageRating,
+        UserId userId,
+        DateTime createdDateTime,
+        DateTime updatedDateTime
+        )
+       : base(hostId)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        ProfileImage = profileImage;
+        AverageRating = averageRating;
+        UserId = userId;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public static Host Create(
+        string firstName,
+        string lastName,
+        string profileImage,
+        float averageRating,
+        UserId userId)
+    {
+        return new(
+            HostId.CreateUnique(),
+            firstName,
+            lastName,
+            profileImage,
+            averageRating,
+            userId,
+            DateTime.UtcNow,
+            DateTime.UtcNow);
+    }
+}
---
+++ Restaurant.Domain/HostAggregate/ValueObjects/HostId.cs
@@ -0,0 +1,22 @@
+﻿using Restaurant.Domain.Common.Models;
+
+namespace Restaurant.Domain.Host.ValueObjects;
+
+public sealed class HostId : ValueObject
+{
+    public Guid Value { get; }
+    private HostId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static HostId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
